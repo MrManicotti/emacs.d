70d8ebafe4f994b61611c230c93a7c863d34d1fb
Chris Moore <c.moore@digitalrealty.com>
2022-05-19 13:30:33 -0500

    Update Init with new dependencies

--- init.el
+++ init.el
@@ -16,11 +16,20 @@
     
     ;; gruvbox theme
     gruvbox-theme
+
+    ;; autocomplete
+    helm
    ))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+
+(helm-mode 1)
 
 (load-theme 'gruvbox t)
 (custom-set-variables
